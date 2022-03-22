55c52ae46e7a6cf8f303daa157c3f44547198dd1
Guillaume Varin <guillaumevar31@gmail.com>
2022-03-22 23:49:42 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,78 @@
+<script setup>
+import SimpleKeyboad from './components/SimpleKeyboad.vue'
+import WordRow from './components/WordRow.vue'
+import { reactive, onMounted, computed } from 'vue'
+
+const state = reactive({
+  solution: "books",
+  guesses: ["", "", "", "", "", ""],
+  currentGuessIndex: 0,
+  guessedLetters: {
+    miss: [],
+    found: [],
+    hint: [],
+  }
+})
+
+const wonGame = computed(() => state.guesses[state.currentGuessIndex - 1] === state.solution)
+
+const lostGame = computed(() => !wonGame.value && state.currentGuessIndex >= 6)
+
+const handleInput = key => {
+  if (state.currentGuessIndex >= 6 || wonGame.value) return
+  const currentGuess = state.guesses[state.currentGuessIndex]
+
+  if (key == "{enter}") {
+    if (currentGuess.length == 5) {
+      state.currentGuessIndex++
+      for (let i = 0; i < currentGuess.length; i++) {
+        let c = currentGuess.charAt(i)
+        if (c == state.solution.charAt(i)) {
+          state.guessedLetters.found.push(c)
+        } else if (state.solution.indexOf(c) != -1) {
+          state.guessedLetters.hint.push(c)
+        } else {
+          state.guessedLetters.miss.push(c)
+        }
+      }
+    }
+  } else if (key == "{bksp}") {
+    state.guesses[state.currentGuessIndex] = currentGuess.slice(0, -1)
+  } else if (currentGuess.length < 5) {
+    const alphaRegex = /[a-zA-Z]/
+    if (alphaRegex.test(key)) {
+      state.guesses[state.currentGuessIndex] += key
+    }
+  }
+}
+
+onMounted(() => {
+  window.addEventListener('keyup', e => {
+    e.preventDefault()
+    let key =
+      e.key == "Enter"
+        ? "{enter}"
+        : e.key == "Backspace"
+          ? "{bksp}"
+          : e.key.toLowerCase()
+    handleInput(key)
+  })
+})
+</script>
+
+<template>
+  <div class="flex flex-col h-screen max-w-md mx-auto justify-evenly">
+    <div>
+      <word-row
+        v-for="(guess, i) in state.guesses"
+        :key="i"
+        :value="guess"
+        :solution="state.solution"
+        :submitted="i < state.currentGuessIndex"
+      />
+    </div>
+    <p v-if="wonGame" class="text-center">🏆 Félicitations vous l'avez résolu !</p>
+    <p v-else-if="lostGame" class="text-center">😔 À court d'essais.</p>
+    <simple-keyboad @onKeyPress="handleInput" :guessedLetters="state.guessedLetters" />
+  </div>
+</template>
